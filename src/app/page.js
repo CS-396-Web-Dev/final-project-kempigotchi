5e9c0c41a180e9d0e146cd8aa0969f042715187f
Patrick Jiang <patrick.jiang.2020@gmail.com>
2024-12-01 00:14:28 -0800

    refactored happiness hearts into separate component

--- src/app/page.js
+++ src/app/page.js
@@ -16,6 +16,7 @@ import SignOutButton from "./components/SignOutButton";
 import EditableTitle from "./components/EditableTitle";
 import StatBar from "./components/StatBar";
 import StatActionCounter from "./components/StatActionCounter";
+import StatHearts from "./components/StatHearts";
 
 export default function Home() {
   const [user, setUser] = useState(null); // User state
@@ -357,38 +358,7 @@ export default function Home() {
       </div>
 
       {/* Happiness on right corner */}
-      <div className="absolute top-16 right-8 flex flex-wrap md:flex-nowrap items-center space-x-1">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <span key={index} className="w-8 h-8 md:w-10 md:h-10">
-            {index <
-            Math.round((Number(pet.happiness) / MAX_STAT_VALUE) * 5) ? (
-              // Filled hearts
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                stroke="currentColor"
-                strokeWidth="2"
-                className="w-full h-full text-red-500"
-              >
-                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-              </svg>
-            ) : (
-              // Empty Hearts
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                className="w-full h-full text-red-500"
-              >
-                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-              </svg>
-            )}
-          </span>
-        ))}
-      </div>
+      <StatHearts happiness={pet.happiness} maxStatValue={MAX_STAT_VALUE} />
 
       {/* Pet Image */}
       <div className="flex flex-col items-center justify-center flex-grow relative px-4">
